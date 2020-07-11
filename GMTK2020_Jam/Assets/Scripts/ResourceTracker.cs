d60fafdb5dd03858771cb158e8ba0c85e6bc3e4d
Joel Lee <lee.joel395@gmail.com>
2020-07-11 00:30:03 -0700

    Working code for resource + affiliated meter. WIP for in scene view changer

---
+++ GMTK2020_Jam/Assets/Scripts/ResourceTracker.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// simple tracker for a resource while updating a fill image to represent it
+/// </summary>
+public class ResourceTracker : MonoBehaviour
+{
+    [SerializeField]
+    private string _resourceName = "Will";
+    [SerializeField]
+    private float _value;
+    [SerializeField]
+    private float _valueMax;
+    [SerializeField]
+    private Image _meter;
+
+    private void Start() {
+        UpdateMeter();
+    }
+
+    public void UpdateResource(float delta) { 
+        _value += delta;
+        _value = Mathf.Clamp(_value, 0.0f, _valueMax);
+        if (_meter != null) UpdateMeter();
+    }
+
+    private void UpdateMeter() {
+        _meter.fillAmount = _value / _valueMax;
+    }
+
+}
