d1fd70e00a274a98c6f7700d9192118500175f8c
Joel Lee <lee.joel395@gmail.com>
2020-07-11 18:42:51 -0700

    Phone work

--- GMTK2020_Jam/Assets/Scripts/PhoneTextReciever.cs
+++ GMTK2020_Jam/Assets/Scripts/PhoneTextReciever.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class PhoneTextReciever : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField]
+    private GameObject _phoneObj;
+    [SerializeField]
+    private StoryBeat[] _beats;
+    [SerializeField]
+    private float _messageDelay = 0.5f;
+    
+    [SerializeField]
+    private AudioClip _phonePing;
+    [SerializeField]
+    private AudioSource _phoneAudioSrc;
+
+    private bool _isShowingMessages = false;
+
+    private void Start() {
+        for (int i = 0; i < _beats.Length; i++) {
+            _beats[i].rootObject.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void ShowPhone() {
+        _phoneObj.SetActive(true);
     }
+
+    public void HidePhone() {
+        _phoneObj.SetActive(false);
+        for(int i = 0; i < _beats.Length; i++)  {
+            _beats[i].rootObject.SetActive(false);
+        }
+    }
+
+    public void StartPhoneSequence(int storyBeat) {
+        StartCoroutine(ShowMessages(_beats[storyBeat]));
+    }
+
+    private IEnumerator ShowMessages(StoryBeat beat) {
+        WaitForSeconds delay = new WaitForSeconds(_messageDelay);
+        int count = 0;
+        beat.rootObject.SetActive(false);
+        while (count < beat.messages.Length) {
+            beat.messages[count].SetActive(true);
+            count++;
+            yield return 0.0f;
+        }
+        beat.rootObject.SetActive(false);
+        yield return 0.0f;
+    }
+}
+
+[System.Serializable]
+public class StoryBeat {
+    public GameObject rootObject;
+    public GameObject[] messages;
 }
