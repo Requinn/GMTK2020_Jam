f8e25633fe4a19a167a95c5e45c35e9d420cf1df
Joel Lee <lee.joel395@gmail.com>
2020-07-11 19:09:25 -0700

    Phone text UI

--- GMTK2020_Jam/Assets/Scripts/PhoneTextReciever.cs
+++ GMTK2020_Jam/Assets/Scripts/PhoneTextReciever.cs
@@ -19,9 +19,7 @@ public class PhoneTextReciever : MonoBehaviour
     private bool _isShowingMessages = false;
 
     private void Start() {
-        for (int i = 0; i < _beats.Length; i++) {
-            _beats[i].rootObject.SetActive(false);
-        }
+        HidePhone();
     }
 
     public void ShowPhone() {
@@ -36,19 +34,22 @@ public class PhoneTextReciever : MonoBehaviour
     }
 
     public void StartPhoneSequence(int storyBeat) {
+        if (_isShowingMessages) return;
         StartCoroutine(ShowMessages(_beats[storyBeat]));
     }
 
     private IEnumerator ShowMessages(StoryBeat beat) {
+        _isShowingMessages = true;
         WaitForSeconds delay = new WaitForSeconds(_messageDelay);
         int count = 0;
         beat.rootObject.SetActive(false);
         while (count < beat.messages.Length) {
+            yield return delay;
             beat.messages[count].SetActive(true);
             count++;
             yield return 0.0f;
         }
-        beat.rootObject.SetActive(false);
+        _isShowingMessages = false;
         yield return 0.0f;
     }
 }
